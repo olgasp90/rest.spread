ca9d52dd9bbc4f716bae9137b7d4c6dcb14ca782
Olga <olga_sprincean@yahoo.com>
2021-01-06 16:25:49 -0600

    wrote the functions

--- restspred.js
+++ restspred.js
@@ -7,12 +7,15 @@ function filterOutOdds() {
   }
 
   //Refactor it to use the rest operator and an arrow function
-
+  const filterOutOdds =(...nums) => nums.filter((num) => num % 2 === 0)
 
 //   findMin
 // Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
 
 // Make sure to do this using the rest and spread operator.
+function findMin(...nums){
+    return nums.reduce((min, current) => current < min? current : min)
+}
 
 findMin(1,4,12,-3) // -3
 findMin(1,-1) // -1
@@ -20,10 +23,18 @@ findMin(3,1) // 1
 
 // mergeObjects
 // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+function mergeObjects(obj1,obj2){
+    return {...obj1,...obj2}
+}
+
 mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 
 // doubleAndReturnArgs
 // Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+function doubleAndReturnArgs(arr, ...val ){
+    return [...arr, ...val.map(el => el * 2)]
+}
+
 doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 
@@ -35,40 +46,68 @@ doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 /** remove a random element in the items array
 and return a new array without that item. */
 
-function removeRandom(items) {
-
+// function removeRandom(items) {
+//     let randomItem = Math.floor(Math.random() * items.length)
+//     return [...items.slice(0, randomItem), ...items.slice(randomItem + 1)]
+// }
+//refactored version
+const removeRandom = (items) => {
+    let randomItem = Math.floor(Math.random() * items.length)
+    return [...items.slice(0, randomItem), ...items.slice(randomItem + 1)]
 }
 
 /** Return a new array with every item in array1 and array2. */
 
-function extend(array1, array2) {
-
-}
+// function extend(array1, array2) {
+//     return [...array1,...array2]
+// }
+//refactored:
+const extend = (array1,array2) => [...array1,...array2];
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-function addKeyVal(obj, key, val) {
-
-}
-
+// function addKeyVal(obj, key, val) {
+//     return {...obj, [key]: val}
+// }
+//refactored version:
+const addKeyVal = (obj, key, val) => {
+    const obj2 = {...obj};
+    obj2[key] = val;
+    return obj2;
+};
 
 /** Return a new object with a key removed. */
-
-function removeKey(obj, key) {
-
+// function removeKey(obj, key) {
+//     const obj2 = {...obj};
+//     delete obj2[key];
+//     return obj2;
+// }
+//refactored version:
+const removeKey = (obj,key) => {
+    const obj2 = {...obj};
+    delete obj2[key];
+    return obj2;
 }
-
 
 /** Combine two objects and return a new object. */
 
-function combine(obj1, obj2) {
-
-}
-
+// function combine(obj1, obj2) {
+//     return {...obj1, ...obj2}
+// }
+//refactored version:
+const combine = (obj1, obj2) => {...obj1, ...obj2}
 
 /** Return a new object with a modified key and value. */
 
-function update(obj, key, val) {
-
+// function update(obj, key, val) {
+//     const newObj = {...obj};
+//     newObj[key] = val;
+//     return newObj;
+// }
+//Refactored version:
+const update = (obj, key, val) => {
+    const newObj = {...obj};
+    newObj[key] = val;
+    return newObj;
 }
